cbbefd19001bb9bba4be84814265089c9f37572e
benbarnett <ben.barnett@brushfiredesign.net>
2011-12-06 11:37:04 +0000

    Adding express and setting up site structure

---
+++ site/js/arduino/EventDispatcher.js
@@ -0,0 +1,98 @@
+/**
+ * @author Jeff Hoefs
+ */
+
+ARDJS.namespace('ARDJS.EventDispatcher');
+
+ARDJS.EventDispatcher = (function () {
+
+	var EventDispatcher;
+
+	/**
+	 * An as3-like EventDispatcher class.
+	 *
+	 * @exports EventDispatcher as ARDJS.EventDispatcher
+	 * @constructor
+	 * @param {Class} target The instance of the class that implements EventDispatcher
+	 */
+	EventDispatcher = function(target) {
+		"use strict";
+		
+		this._target = target || null;
+		this._eventListeners = {};
+		
+		this.name = "EventDispatcher"; // for testing	
+	}
+
+	EventDispatcher.prototype = {
+
+		/**
+		 * @param {String} type The event type
+		 * @param {Function} listener The function to be called when the event is fired
+		 */
+		addEventListener: function (type, listener) {
+			if(!this._eventListeners[type]) {
+				this._eventListeners[type] = [];
+			}
+			this._eventListeners[type].push(listener);
+		},
+		
+		/**
+		 * @param {String} type The event type
+		 * @param {Function} listener The function to be called when the event is fired
+		 */
+		removeEventListener: function(type, listener) {
+			for (var i=0, len = this._eventListeners[type].length; i<len; i++) {
+				if (this._eventListeners[type][i] == listener) {
+					this._eventListeners[type].splice(i, 1);
+				}
+			}
+		},
+		
+		/**
+		 * @param {String} type The event type
+		 * return {boolean} True is listener exists for this type, false if not.
+		 */
+		hasEventListener: function(type) {
+			// to do: implement this method
+			if (this._eventListeners[type]) {
+				return true;
+			} else {
+				return false;
+			}	
+		},
+		
+		/**
+		 * @param {Event} type The Event object.
+		 * @param {Object} optionalParams Optional parameters passed as an object.
+		 * return {boolean} True if dispatch is successful, false if not.
+		 */	
+		dispatchEvent: function(event, optionalParams) {
+			
+			event.target = this._target;
+			var isSuccess = false;		
+
+			// add any optional params to the Event object
+			for (var obj in optionalParams) {
+				event[obj.toString()] = optionalParams[obj];
+			}
+						
+			if (this.hasEventListener(event.type)) {
+				for (var j=0, len=this._eventListeners[event.type].length; j<len; j++) {
+					try {
+						this._eventListeners[event.type][j].call(this, event);
+						isSuccess = true;
+					} catch(e) {
+						// to do: handle error
+					}
+				}
+			}
+			return isSuccess;	
+		}
+
+
+	}
+
+	return EventDispatcher;
+
+}());
